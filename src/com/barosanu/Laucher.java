aa5d133e221d87de79971fa5f5ef11a39e105039
Andrzej Konicki <Andrzej.konicki.programista@gmail.com>
2021-05-06 20:44:51 +0200

    Initial commit

---
+++ src/com/barosanu/Laucher.java
@@ -0,0 +1,17 @@
+package com.barosanu;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Laucher extends Application{
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage arg0) throws Exception {
+		stage.show();
+	}	
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,6 @@
+module JavaFxEmailClient {
+	requires javafx.fxml;
+	requires javafx.controls;
+	requires javafx.graphics;
+	requires javafx.web;
+}
